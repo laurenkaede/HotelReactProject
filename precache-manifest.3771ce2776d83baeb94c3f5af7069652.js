da41a3e8a420693ec2028d05178b74c429d9988f
Lauren Moor <laurenkaede@gmail.com>
2020-03-29 15:54:03 +0100

    Updates

---
+++ precache-manifest.3771ce2776d83baeb94c3f5af7069652.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d79b6c5a28b4fc104afc2a69ba1f91c1",
+    "url": "/https//laurenkaede.github.io/HotelReactProject/index.html"
+  },
+  {
+    "revision": "bcab639e6b308cc5377c",
+    "url": "/https//laurenkaede.github.io/HotelReactProject/static/css/main.5c4fb615.chunk.css"
+  },
+  {
+    "revision": "b3e0414d7c382fbe380e",
+    "url": "/https//laurenkaede.github.io/HotelReactProject/static/js/2.66ecc77d.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/https//laurenkaede.github.io/HotelReactProject/static/js/2.66ecc77d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "bcab639e6b308cc5377c",
+    "url": "/https//laurenkaede.github.io/HotelReactProject/static/js/main.14458d6d.chunk.js"
+  },
+  {
+    "revision": "276aa4279b75abbfc44c",
+    "url": "/https//laurenkaede.github.io/HotelReactProject/static/js/runtime-main.e7a18536.js"
+  },
+  {
+    "revision": "2ab168b39b0fca2f152b552f5e1b832e",
+    "url": "/https//laurenkaede.github.io/HotelReactProject/static/media/person1.2ab168b3.png"
+  },
+  {
+    "revision": "b03621aa4ec856086444b1654b1ed7de",
+    "url": "/https//laurenkaede.github.io/HotelReactProject/static/media/person2.b03621aa.png"
+  },
+  {
+    "revision": "a9dc8e5e6f8896dbba4dd01076a8ffc9",
+    "url": "/https//laurenkaede.github.io/HotelReactProject/static/media/person3.a9dc8e5e.png"
+  },
+  {
+    "revision": "4a418ae2543599dbe6a31da1e1ab521d",
+    "url": "/https//laurenkaede.github.io/HotelReactProject/static/media/place1.4a418ae2.png"
+  },
+  {
+    "revision": "c553455d0858f1af81624bd608b46e2b",
+    "url": "/https//laurenkaede.github.io/HotelReactProject/static/media/place2.c553455d.png"
+  },
+  {
+    "revision": "7319af7a2473ad97f99bf6a5d449391e",
+    "url": "/https//laurenkaede.github.io/HotelReactProject/static/media/place3.7319af7a.png"
+  }
+]);
---
+++ service-worker.js
@@ -0,0 +1,39 @@
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
+
+importScripts(
+  "/https//laurenkaede.github.io/HotelReactProject/precache-manifest.3771ce2776d83baeb94c3f5af7069652.js"
+);
+
+self.addEventListener('message', (event) => {
+  if (event.data && event.data.type === 'SKIP_WAITING') {
+    self.skipWaiting();
+  }
+});
+
+workbox.core.clientsClaim();
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
+
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/https//laurenkaede.github.io/HotelReactProject/index.html"), {
+  
+  blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
+});
